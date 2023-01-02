fe2081e373f7c69030ad84e4c5d39abcc5b89b67
Mark Hessling <mark@rexx.org>
2023-01-03 05:37:43 +1000

    Bump sha256

--- Formula/the-x11w.rb
+++ Formula/the-x11w.rb
@@ -2,7 +2,7 @@ class TheX11w < Formula
   desc "The Hessling Editor (the); x11w variant"
   homepage "https://hessling-editor.sourceforge.io"
   url "https://www.rexx.guru/the-4.0.tar.gz"
-  sha256 "26140f7e75c6ae1a9c07bd379b0295db5e4f9eb9d0f140185a904d8a13b4690a"
+  sha256 "66320a25bcfdf2ac9890a6be2117a9503419d08997c8aec2ede533912d6de6b4"
   license "GPL-2.0-only"
 
   depends_on "pkg-config"
