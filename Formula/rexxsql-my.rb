f3359d30d1d5ec5b31bd9722e9c32c58d430e481
Mark Hessling <mark@rexx.org>
2023-02-05 16:53:01 +1000

    Only install db-specific files so we don't get clashes

--- Formula/rexxsql-my.rb
+++ Formula/rexxsql-my.rb
@@ -2,7 +2,7 @@ class RexxsqlMy < Formula
   desc "Rexx interface to mySQL"
   homepage "https://rexxsql.sourceforge.io"
   url "https://www.rexx.org/packages/rexxsql-2.7.0.tar.gz"
-  sha256 "777f614732374ac3b03a28f366a509935c126eb9feaa9b22dbc984a2900a648a"
+  sha256 "41f683fa37e6cec77fc5e1b02c3756b958054823ea280aef9ea821177ad172e2"
   license "GPL-2.0-only"
 
   depends_on "regina-rexx"
@@ -17,7 +17,7 @@ class RexxsqlMy < Formula
            "--with-arch=none",
            "--with-db=mysql",
            "--with-brew-addon-dir=#{lib}/regina-rexx/addons"
-    system "make", "installbase", "installaddonslink"
+    system "make", "installdbtools", "installdbaddons", "installdbaddonslink"
   end
 
   test do
