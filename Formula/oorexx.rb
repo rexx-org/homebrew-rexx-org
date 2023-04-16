81c41c15c37bdb924b6e5421cf6ee92a88785b37
Mark Hessling <mark@rexx.org>
2023-04-16 12:07:29 +1000

    Add build dependency on cmake

--- Formula/oorexx.rb
+++ Formula/oorexx.rb
@@ -1,23 +1,20 @@
 class Oorexx < Formula
   desc "Open Object Rexx (ooRexx)"
-#  keg_only "So as to not clash with files in Regina with same name"
+  #  keg_only "So as to not clash with files in Regina with same name"
   homepage "https://www.oorexx.org"
   url "https://www.rexx.org/packages/oorexx-5.0.0-12583.tar.gz"
   sha256 "7a37c166dc5ccc0e021f1f1ec93172b5d00c20faa9d688fea31fb8e836501aa7"
   license "CPL-1.0"
 
+  depends_on "cmake" => :build
   depends_on "ncurses"
-  conflicts_with "regina-rexx", because: "Only Regina or ooRexx can be active at the same time"
+  conflicts_with "regina-rexx", because: "only Regina or ooRexx can be active at the same time"
 
   def install
     ENV.deparallelize
-    system "cmake",
-           "-G", "Unix Makefiles",
-           "-S", ".",
-           "-B", "./build",
-           *std_cmake_args
+    system "cmake", "-S", ".", "-B", "./build", *std_cmake_args, "-Wno-error=dev"
     cd "./build" do
-       system "make", "install"
+      system "make", "install"
     end
   end
 
