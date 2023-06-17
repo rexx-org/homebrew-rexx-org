24483707fb32ee942f7d13d1f034e7f38d117028
Mark Hessling <mark@rexx.org>
2023-06-18 08:51:13 +1000

    Switch interpreters with all argument

--- Formula/switchrexx.rb
+++ Formula/switchrexx.rb
@@ -1,8 +1,8 @@
 class Switchrexx < Formula
   desc "Utility to switch the active Rexx Interpreter"
   homepage "https://switchrexx.sourceforge.io"
-  url "https://www.rexx.org/packages/switchrexx-1.0.0.tar.gz"
-  sha256 "df83adf4cf6f243f532c139b08e928c160068b05b910b120bc8a4687fd26fc99"
+  url "https://www.rexx.org/packages/switchrexx-1.0.1.tar.gz"
+  sha256 "ad3cab45b16962ca0b7107b3478e486109cae0107533f306aaf7f3a79439128a"
   license "CPL-1.0"
 
   def install
